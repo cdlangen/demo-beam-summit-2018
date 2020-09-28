c86f578cd928a0ab385580431c9ce515a82904d5
carolyn.langen <carolyn@almende.org>
2020-09-28 14:48:51 +0200

    WIP: use updated versions of flink and beam

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-import argparse
 import logging
 import re
 
@@ -9,7 +8,6 @@ from apache_beam.transforms import window
 from apache_beam.transforms import trigger
 from apache_beam.io.external.kafka import ReadFromKafka
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.transforms.trigger import AccumulationMode
 
 
@@ -17,25 +15,34 @@ class LoggingDoFn(beam.DoFn):
     def process(self, element):
         logging.info(element)
 
-def run(argv=None):
-    parser = argparse.ArgumentParser()
-    known_args, pipeline_args = parser.parse_known_args(argv)
 
-    pipeline_options = PipelineOptions(pipeline_args)
-    pipeline_options.view_as(SetupOptions).save_main_session = True
-    p = beam.Pipeline(options=pipeline_options)
+def run():
+    options = PipelineOptions([
+        "--runner=PortableRunner",
+        "--job_endpoint=localhost:8099",
+        "--environment_type=LOOPBACK"
+    ])
+    # options = PipelineOptions([
+    #     "--runner=FlinkRunner",
+    #     "--flink_master=localhost:8081",
+    # ])
+    with beam.Pipeline(options=options) as p:
+        (p | 'ReadFromKafka' >> ReadFromKafka(consumer_config={"bootstrap.servers": "localhost:9092"},
+                                              topics=["beam-input"])
+        | 'ExtractWords' >> beam.FlatMap(lambda kv: re.findall(r'[A-Za-z\']+', kv[1]))
+        | 'Window' >> beam.WindowInto(window.GlobalWindows(), trigger=trigger.Repeatedly(trigger.AfterCount(1)),
+                                      accumulation_mode=AccumulationMode.ACCUMULATING)
+        | 'Count' >> beam.combiners.Count.PerElement()
+        | 'Format' >> beam.Map(lambda word_count: '%s: %s' % (word_count[0], word_count[1]))
+         | 'Log' >> beam.ParDo(LoggingDoFn()))
 
-    (p | 'ReadFromKafka' >> ReadFromKafka(consumer_config={"bootstrap.servers": "localhost:9092"},
-                                          topics=["beam-input"])
-     | 'ExtractWords' >> beam.FlatMap(lambda (k,v): re.findall(r'[A-Za-z\']+', v))
-     | 'Window' >> beam.WindowInto(window.GlobalWindows(), trigger=trigger.Repeatedly(trigger.AfterCount(1)), accumulation_mode=AccumulationMode.ACCUMULATING)
-     | 'Count' >> beam.combiners.Count.PerElement()
-     | 'Format' >> beam.Map(lambda word_count: '%s: %s' % (word_count[0], word_count[1]))
-     | 'Log' >> beam.ParDo(LoggingDoFn()))
+        # result = p.run()
+        # result.wait_until_finish()
 
-    result = p.run()
-    result.wait_until_finish()
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     run()
+    # import os
+    #
+    # print("{}:{}".format(os.getegid(), os.geteuid()))
